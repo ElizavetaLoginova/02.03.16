8d6388f811486acba7a31e1b7a4ed4c6251deae4
ElizavetaLoginova <elizyk@mail.ru>
2016-03-09 08:28:40 +0300

    Create FindFirstEnter.cpp

---
+++ FindFirstEnter.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+char* FindFirstEnter(char*, int);
+
+int main()
+{
+	while (true)
+	{
+		const int length = 256;
+		cout << "Your text is: " << endl;
+		char* string = "If your ! mouse is unava or; 12345 %& *&"
+			" if you - prefer to use your keyboard, "
+			" you can use the followin keyboard... ";
+		cout << string << endl;
+
+		int n;
+		cout << "Enter the number of elements: " << endl;
+		cin >> n;
+
+		cout << FindFirstEnter(string, n);
+
+		char yes;
+		cout << "Would you like to countinie? Press y/Y: " << endl;
+		cin >> yes;
+		if (yes == 'y' || yes == 'Y')
+			continue;
+		break;
+	}
+	return 0;
+}
+
+char* FindFirstEnter(char*str, int len)
+{
+	char* symbols = "ABCDEFGHIJKLMNOPQRSTUWVXYZabcdefghijklnmopqrstuwvxyz";
+	char* pWord = strpbrk(str, symbols);
+	char* arrayOfWords = "";
+	char *word;
+	while (pWord)
+	{
+		int length = strspn(pWord, symbols);
+		if (length == len)
+		{
+			word = new char[len + 1];
+			word[len] = '\0';
+			word = strncpy(word, pWord, len);
+			pWord += len;
+			arrayOfWords = word;
+			arrayOfWords += (len+1);
+
+		}
+		else
+		{
+			pWord += length;
+			pWord = strpbrk(pWord, symbols);
+		}
+	}
+	return arrayOfWords;
+}
